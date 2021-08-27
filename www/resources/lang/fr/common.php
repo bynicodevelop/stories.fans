3e42126986fc30df5a746a2d6438fa7c59376ce9
nico-develop <contact@nico-develop.fr>
2021-08-27 08:05:20 +0200

    Fixed #17

--- www/resources/lang/fr/common.php
+++ www/resources/lang/fr/common.php
@@ -4,4 +4,7 @@ return [
     "annually" => "annuellement",
     "quarterly" => "tous les trimestres",
     "all-rights-reserved" => "Tous droits réservés",
+    "link_agree" => "J'ai lu et accepté les :terms_of_service et :privacy_policy",
+    "terms_of_service" => "conditions générales d'utilisation",
+    "privacy_policy" => "les politiques de confidentialité",
 ];
