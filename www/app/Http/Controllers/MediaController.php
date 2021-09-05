bbb55d631732d88de90b473a2fc25c4e280a59ab
nico-develop <contact@nico-develop.fr>
2021-09-05 08:50:20 +0200

    Protect content to disable context menu

--- www/app/Http/Controllers/MediaController.php
+++ www/app/Http/Controllers/MediaController.php
@@ -23,6 +23,9 @@ class MediaController extends Controller
 
     public function index(Request $request, $id)
     {
+        set_time_limit(0);
+        ini_set('memory_limit', '4G');
+
         $isBlurred = false;
 
         /**
@@ -30,57 +33,75 @@ class MediaController extends Controller
          */
         $user = Auth::user();
 
-        try {
-            extract($this->mediaService->blurred($user, $id));
+        // try {
+        extract($this->mediaService->blurred($user, $id));
 
-            try {
-                $this->authorize('seePost', $post);
-            } catch (Exception $e) {
-                $isBlurred = true;
+        try {
+            $this->authorize('seePost', $post);
+        } catch (Exception $e) {
+            $isBlurred = true;
+        }
+
+        $storagePath = Storage::get("private/{$name}.{$ext}"); // storage_path("app/private/{$name}.{$ext}");
+
+        if ($type == Media::IMAGE) {
+            $image = Image::make($storagePath);
+
+            if ($isBlurred) {
+                $image->blur(60);
             }
 
-            $storagePath = Storage::get("private/{$name}.{$ext}"); // storage_path("app/private/{$name}.{$ext}");
+            return $image->response();
+        } else {
+            extract($this->mediaService->blurred($user, $id));
 
-            if ($type == Media::IMAGE) {
-                $image = Image::make($storagePath);
+            $isPreview = $request->input('preview');
+
+            if (!is_null($isPreview)) {
+                $previewStoragePath = Storage::get("private/{$name}-preview.jpg");
+
+                $image = Image::make($previewStoragePath);
 
                 if ($isBlurred) {
-                    $image->blur(60);
+                    $image->pixelate(40);
                 }
 
                 return $image->response();
-            } else {
-                extract($this->mediaService->blurred($user, $id));
-
-                $isPreview = $request->input('preview');
-
-                if (!is_null($isPreview)) {
-                    $previewStoragePath = Storage::get("private/{$name}-preview.jpg");
-
-                    $image = Image::make($previewStoragePath);
-
-                    if ($isBlurred) {
-                        $image->pixelate(40);
-                    }
-
-                    return $image->response();
-                }
             }
-
-            $stream = Storage::readStream("private/{$name}.{$ext}");
-
-            return response()->stream(function () use ($stream) {
-                fpassthru($stream);
-            }, 200, [
-                "Content-Type" => "video/mp4",
-                "Cache-Control" => "max-age=2592000, public",
-                "Expires" => gmdate('D, d M Y H:i:s', time() + 2592000) . ' GMT',
-            ]);
-        } catch (Exception $e) {
-            Log::error("MediaController: ", [
-                "message" => $e->getMessage()
-            ]);
-            abort(404);
         }
+
+        dd($storagePath);
+        $fs = Storage::getDriver();
+
+        $metaData = $fs->getMetadata($storagePath);
+        $stream = $fs->readStream($storagePath);
+
+        if (ob_get_level()) ob_end_clean();
+
+        return response()->stream(
+            function () use ($stream) {
+                fpassthru($stream);
+            },
+            200,
+            [
+                'Content-Type' => $metaData['type'],
+                'Content-disposition' => 'attachment; filename="' . $metaData['path'] . '"',
+            ]
+        );
+        // $stream = Storage::readStream("private/{$name}.{$ext}");
+
+        // return response()->stream(function () use ($stream) {
+        //     fpassthru($stream);
+        // }, 200, [
+        //     "Content-Type" => "video/mp4",
+        //     "Cache-Control" => "max-age=2592000, public",
+        //     "Expires" => gmdate('D, d M Y H:i:s', time() + 2592000) . ' GMT',
+        // ]);
+        // } catch (Exception $e) {
+        //     Log::error("MediaController: ", [
+        //         "message" => $e->getMessage()
+        //     ]);
+        //     abort(404);
+        // }
     }
 }
--- www/resources/js/app.js
+++ www/resources/js/app.js
@@ -4,15 +4,26 @@ require("./prism");
 import Alpine from "alpinejs";
 import Echo from "laravel-echo";
 
-if (window.configJS != undefined) {
-    window.Echo = new Echo({
-        broadcaster: "socket.io",
-        host: window.configJS.websocket_url,
-    });
+if (window.configJS == undefined) {
+    window.configJS = {
+        websocket_url: null,
+        disable_context_menu: "production",
+    };
 }
+
+window.Echo = new Echo({
+    broadcaster: "socket.io",
+    host: window.configJS.websocket_url,
+});
 
 window.Alpine = Alpine;
 
 Alpine.start();
 
 Prism.highlightAll();
+
+if (window.configJS.disable_context_menu == true) {
+    document.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+    });
+}
