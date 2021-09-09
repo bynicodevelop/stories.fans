ef8c6c6c49a6b6a71b7e83e78581381ce3719321
nico-develop <contact@nico-develop.fr>
2021-09-09 08:39:10 +0200

    Updated the config of supervisord

--- www/config/filesystems.php
+++ www/config/filesystems.php
@@ -76,6 +76,7 @@ return [
             'endpoint' => env('DO_SPACES_ENDPOINT'),
             'region' => env('DO_SPACES_REGION'),
             'bucket' => env('DO_SPACES_BUCKET'),
+            'visibility' => 'public',
         ],
 
     ],
