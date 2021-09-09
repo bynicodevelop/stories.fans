39b2f724a8a3b05dd599438efb88e2ab1a472555
nico-develop <contact@nico-develop.fr>
2021-09-09 11:27:00 +0200

    Updated shares scripts components

--- www/resources/views/livewire/post/copy-button.blade.php
+++ www/resources/views/livewire/post/copy-button.blade.php
@@ -1,20 +1,18 @@
-<button type="button" id="share-button-{{ $post['id'] }}" class="inline-block px-2 py-2 text-center"
-    data-clipboard-text="{{ route('post', ['postId' => $post['id']]) }}">
-    @if ($hasCopy)
-        <i class="fas fa-share-alt text-pink-500 pr-1"></i>
-    @else
-        <i class="fas fa-share-alt pr-1"></i>
-    @endif
-</button>
+<div>
+    <button type="button" id="share-button-{{ $post['id'] }}" class="inline-block px-2 py-2 text-center"
+        data-clipboard-text="{{ route('post', ['postId' => $post['id']]) }}">
+        @if ($hasCopy)
+            <i class="fas fa-share-alt text-pink-500 pr-1"></i>
+        @else
+            <i class="fas fa-share-alt pr-1"></i>
+        @endif
+    </button>
 
-@push('scripts')
     <script type="application/javascript">
-        document.addEventListener('DOMContentLoaded', function() {
-            let clipboard = new ClipboardJS('#share-button-{{ $post['id'] }}');
+        let clipboard = new ClipboardJS('#share-button-{{ $post['id'] }}');
 
-            clipboard.on('success', function(e) {
-                window.livewire.emit('copied')
-            });
-        })
+        clipboard.on('success', function(e) {
+            window.livewire.emit('copied')
+        });
     </script>
-@endpush
+</div>
